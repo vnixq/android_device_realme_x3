620d700e716dfd7165600c66371991fb55eefcdb
Vansh Sethi <vanshsethi2304@gmail.com>
2024-04-02 03:20:01 +0530

    derpify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_x3.mk
+    $(LOCAL_DIR)/derp_x3.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_x3-user \
-    blaze_x3-userdebug \
-    blaze_x3-eng
+    derp_x3-user \
+    derp_x3-userdebug \
+    derp_x3-eng
--- derp_x3.mk
+++ derp_x3.mk
@@ -20,21 +20,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/realme/x3/device.mk)
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-# superior
-TARGET_INCLUDE_PIXEL_CHARGER := true
+# derp
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-SUPERIOR_OFFICIAL := false 
-TARGET_SUPPORTS_FACE_UNLOCK := true
-TARGET_SUPPORTS_BLUR := true
-SYSTEM_OPTIMIZE_JAVA := true
-SYSTEMUI_OPTIMIZE_JAVA := true
-BUILD_WITH_GAPPS := true
+IS_PHONE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+DERP_BUILDTYPE := Personal
+
+# Enable quick tap
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier
-PRODUCT_NAME := superior_x3
+PRODUCT_NAME := derp_x3
 PRODUCT_DEVICE := x3
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme X3 / SuperZoom
